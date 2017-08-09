75d18492258246c3a46bab80740ffad2a38d70c5
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2017-08-09 15:04:36 -0700

    Adds a README and contributing information

---
+++ database/apiHelper.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// var request = require('request');
+
+// create Mongoose schema to hold images and likes
+
+var picSchema = new Schema({
+  original: String,
+  likes: Array
+});
+
+// create model
+
+// server responds to POST request by saving to database in the correct schema
+
+var savePic = function(data, callback) {
+
+}
