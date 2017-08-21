bb3d4d37d092c3b147ff2b83782dcf5a12ce0502
Pavan Sethi <pavansethi@Pavans-MacBook-Pro.local>
2017-08-21 12:28:25 -0700

    Switches to correct file names

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,182 +1,158 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import Dropzone from 'react-dropzone';
 
 import data from './dummy_data.js';
-import ImageList from './components/ImageList.jsx';
-import SavedList from './components/SavedList.jsx';
-
-// TODO: We need to get this collection from Mongoose
-const PictureCollection = 'TODO'
-
-// function onFileUploaded(acceptedFiles, rejectedFiles) {
-//   acceptedFiles.forEach(file => {
-//         const reader = new FileReader();
-//         reader.onload = () => {
-//             const fileAsBinaryString = reader.result;
-//             // Store the file content in the database
-//             const newPicture = {
-//               name: 'GET THIS FROM FILE READER',
-//               content: fileAsBinaryString,
-//             }
-//             // PictureCollection.insert(newPicture)
-//             // Do error handling with callback or promise
-
-//         };
-//         reader.onabort = () => console.log('file reading was aborted');
-//         reader.onerror = () => console.log('file reading has failed');
-
-//         reader.readAsBinaryString(file);
-//     });
-// }
+// import ImageList from './components/ImageList.jsx';
+// import SavedList from './components/SavedList.jsx';
+// http://www.michiganduckrescueandsanctuary.com/wp-content/uploads/2014/12/marold_donate_transparent_504.png
+// https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg
+// const originalImgUrl = {"imageUri": "https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg"};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      guess: undefined,
-      data: data[0],
-      saved: []
+      something: '',
+      // twentyFiveUrls: [],
+      twentyFiveUrls: data,
+      originImgUrl: {"imageUri": "https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg"},
+      userInput: '',
+      currentImgRowIdx: 0
     };
-    this.saveImage = this.saveImage.bind(this);
+    this.postOriginalImg = this.postOriginalImg.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.showNextRow = this.showNextRow.bind(this);
+    this.showNextRow = this.showNextRow.bind(this);
+    this.showPrevRow = this.showPrevRow.bind(this);
   }
-
 
   /***ComponentDidmount
   Will make an ajax call to the server, which will grab all the saved images for the user, then render that to the user via setState.
   ***/
   componentDidMount() {
-    console.log('Component Mounted!', data);
-    // let that = this;
-    // // Get all saved items from the DB
+    // this.postOriginalImg(this.state.originImgUrl);
+  }
 
-    // // Query the  database for all pictures
-    // PictureCollection.find({}, (pictureDocuments) => {
-    //   // pictureDocuments = [{ url }, { url }, ...]
-    //   this.setState({
-    //     // TODO: Think about how to structure this for storage in the state
-    //     saved: pictureDocuments
-    //   })
-    // })
-
-
-    /*$.ajax({
-      method: 'GET',
-      url: '/user',
-      success: function(results) {
-        console.log('Client Get Success', results);
-        that.setState({
-          saved: results
-        });
+  postOriginalImg(originalImgUrl) {
+    $.ajax({
+      method: 'POST',
+      url: '/images',
+      data: originalImgUrl,
+      success: (results) => {
+        console.log('Client POST Original Img Success', results);
+        this.setState({
+          twentyFiveUrls: results
+        })
       },
       error: function(err) {
-        console.log('Client Err', err);
+        console.log('Client POST Err', err);
       }
-    });*/
-
+    })
   }
 
-  /***
-    Adds Image to the Saved Array, upon click of button, we will send a POST request to the server to add this image to the DB.
-  ***/
-
-  saveImage(image) {
-    // var newImage = event.target.value;
-    /*console.log('Saved!', image);
-    var newArray = this.state.saved.slice();
-    newArray.push(image);
-
-    let that = this;
-      $.ajax({
-        method: 'POST',
-        url: '/user',
-        data: image,
-        success: function(results) {
-          console.log('Client Post Success', results);
-          that.setState({
-            saved: newArray
-          });
-        },
-        error: function(err) {
-          console.log('Client Err', err);
-        }
-      });
-      */
-
-      // TODO: Get binary data or base64 from image
-      // PictureCollection.insert(newPicture)
-      // Do error handling with callback or promise
-
-
-
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    })
   }
 
+  handleClick() {
+    var originImgUrl = {imageUri: this.state.userInput};
+    console.log('click ', originImgUrl)
+    this.setState({
+      originImgUrl: originImgUrl
+    });
+    // this.postOriginalImg(originImgUrl);
+  }
+
+  showPrevRow() {
+    
+  }
+
+  showNextRow() {
+
+  }
 
   render() {
-    console.log('App Render Triggered')
     return (
       <div>
-        <h1>Comp</h1>
-        <button onClick={this.onFileUploaded}>Upload picture</button>
-        <ImageUpload/>
-        {/* need to style this as a file dropzone */}
-        <div onDrop={onFileUploaded}></div>
-        <div className="app">
-          <ImageList list={this.state.data} saveImage={this.saveImage} pictures={this.pictures}/>
-          <div className="category">
-            <h3>Saved</h3>
-            <SavedList saved={this.state.saved}/>
-          </div>
 
+      <div className="well" id="title"><h1>ImgNest</h1></div>
+      
+      <div className="container"> 
+        <div className="row">
+          <div className="form-group">
+
+            <div className="col-md-6 col-md-offset-2 col-lg-6 col-lg-offset-2">
+              <input 
+                type="text" 
+                className="form-control input-lg" 
+                value={this.userInput}
+                placeholder="Search for..."
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <p className="btn btn-success btn-lg" onClick={this.handleClick}> GO !</p>
+            </div>
+
+          </div>
         </div>
+      </div> 
+
+
+      <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="thumbnail">
+                <img src={this.state.originImgUrl.imageUri} />
+                <div className="caption">
+                  {/* <h3>Thumbnail label</h3> */}
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
+
+       <div className="well"></div> 
+      {/* <hr/> */}
+
+      
+    
+      <div className="row">
+      <div className="col-md-1 col-lg-1">
+        {/* <p className="btn btn-info btn-lg"><span className="glyphicon glyphicon-search"></span></p> */}
+        <p className="btn btn-info btn-lg"><span className="glyphicon glyphicon-arrow-down"></span></p>
+      </div>
+      {this.state.twentyFiveUrls.map((guessAndUrls) => {
+        return (
+          <div className="col-md-2 col-lg-2">
+              <div className="thumbnail">
+                <img src={guessAndUrls.urls[this.state.currentImgRowIdx]} />
+                <div className="row">
+                  <div className="col-md-2 col-lg-2"/>
+                   <div className="col-md-4 col-lg-4"> <a href="#" className="btn btn-primary" role="button">Like !</a></div> 
+                  <div className="col-md-4 col-lg-4"> <a href="#" className="btn btn-success" role="button">Go !</a></div>
+                </div>
+              </div>
+            </div>
+        );
+      })}
+      <div className="col-md-1 col-lg-1">
+        {/* <p className="btn btn-info btn-lg"><span className="glyphicon glyphicon-search"></span></p> */}
+        <p 
+        className="btn btn-info btn-lg"
+        onClick={this.showNextRow}
+        ><span className="glyphicon glyphicon-arrow-up"></span></p>
+      </div>
+      </div> {/* end of row  */}
+    </div> 
+      
     );
   }
 }
-
-
-
-const handleDropRejected = (...args) => console.log('reject', args)
-
- const onFileUploaded = (...args) => {
-    console.log('inside onFileUploaded', args)
-  }
-
-class ImageUpload extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { preview: null }
-    this.handleDrop = this.handleDrop.bind(this)
-  }
-
-
-
-
-  handleDrop([{ preview }]) {
-    console.log('preview', preview)
-    this.setState({ preview })
-  }
-
-  render() {
-    const { preview } = this.state
-
-    return (
-
-      <section>
-        <Dropzone onDrop={ this.handleDrop } accept="image/jpeg,image/jpg,image/tiff,image/gif" multiple={ false } onDropRejected={ handleDropRejected } >
-          Drag a file here or click to upload.
-        </Dropzone>
-        { preview &&
-        <img src={ preview } alt="image preview" />
-        }
-      </section>
-    )
-  }
-}
-
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
