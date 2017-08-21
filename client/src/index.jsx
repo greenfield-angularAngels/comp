0bd1a3ad25d8b6d8a60c12dd744c13d229d43385
sean0923 <iamseanhong@gmail.com>
2017-08-21 01:11:04 -0700

    Able to bring five pics from original pics

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,81 +5,126 @@ import $ from 'jquery';
 import data from './dummy_data.js';
 import ImageList from './components/ImageList.jsx';
 import SavedList from './components/SavedList.jsx';
+// http://www.michiganduckrescueandsanctuary.com/wp-content/uploads/2014/12/marold_donate_transparent_504.png
+// https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg
+// const originalImgUrl = {"imageUri": "https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg"};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      guess: undefined,
-      data: data[0],
-      saved: []
+      something: '',
+      twentyFiveUrls: [],
+      originImgUrl: {"imageUri": "https://s4.favim.com/orig/48/puppy-lion-cute-Favim.com-445038.jpg"},
+      userInput: ''
     };
-    this.saveImage = this.saveImage.bind(this);
+    this.postOriginalImg = this.postOriginalImg.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   /***ComponentDidmount
   Will make an ajax call to the server, which will grab all the saved images for the user, then render that to the user via setState.
   ***/
   componentDidMount() {
-    console.log('Component Mounted!', data);
-    let that = this;
-    // Get all saved items from the DB
-    $.ajax({
-      method: 'GET',
-      url: '/user', 
-      success: function(results) {
-        console.log('Client Get Success', results);
-        that.setState({
-          saved: results
-        });
-      },
-      error: function(err) {
-        console.log('Client Err', err);
-      }
-    });
+    this.postOriginalImg(this.state.originImgUrl);
   }
 
-  /***
-    Adds Image to the Saved Array, upon click of button, we will send a POST request to the server to add this image to the DB.
-  ***/
-  saveImage(image) {  
-    // var newImage = event.target.value; 
-    console.log('Saved!', image);
-    var newArray = this.state.saved.slice();
-    newArray.push(image);
+  postOriginalImg(originalImgUrl) {
+    $.ajax({
+      method: 'POST',
+      url: '/images',
+      data: originalImgUrl,
+      success: (results) => {
+        console.log('Client POST Original Img Success', results);
+        this.setState({
+          twentyFiveUrls: results
+        })
+      },
+      error: function(err) {
+        console.log('Client POST Err', err);
+      }
+    })
+  }
 
-    let that = this;
-      $.ajax({
-        method: 'POST',
-        url: '/user',
-        data: image,
-        success: function(results) {
-          console.log('Client Post Success', results);
-          that.setState({
-            saved: newArray
-          });
-        },
-        error: function(err) {
-          console.log('Client Err', err);
-        }
-      });
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    })
+  }
 
+  handleClick() {
+    var originImgUrl = {imageUri: this.state.userInput};
+    console.log('click ', originImgUrl)
+    this.setState({
+      originImgUrl: originImgUrl
+    });
+    this.postOriginalImg(originImgUrl);
   }
 
   render() {
-    console.log('App Render Triggered')
     return (
       <div>
-        <h1>Comp</h1>
-        <div className="app">
-          <ImageList list={this.state.data} saveImage={this.saveImage}/>
-          <div className="category">
-            <h3>Saved</h3>
-            <SavedList saved={this.state.saved}/>
-          </div>
 
+      <div className="well" id="title"><h1>ImgNest</h1></div>
+      
+      <div className="container"> 
+        <div className="row">
+          <div className="form-group">
+
+            <div className="col-md-6 col-md-offset-2 col-lg-6 col-lg-offset-2">
+              <input 
+                type="text" 
+                className="form-control input-lg" 
+                value={this.userInput}
+                placeholder="Search for..."
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <p className="btn btn-success btn-lg" onClick={this.handleClick}> GO !</p>
+            </div>
+
+          </div>
         </div>
+      </div> 
+
+
+      <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="thumbnail">
+                <img src={this.state.originImgUrl.imageUri} />
+                <div className="caption">
+                  {/* <h3>Thumbnail label</h3> */}
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
+
+       <div className="well"></div> 
+      {/* <hr/> */}
+
+      <div className="col-md-1 col-lg-1"/>
+    
+      <div className="row">
+      {this.state.twentyFiveUrls.map((guessAndUrls) => {
+        return (
+          <div className="col-md-2 col-lg-2">
+              <div className="thumbnail">
+                <img src={guessAndUrls.urls[1]} />
+                <div className="row">
+                  <div className="col-md-2 col-lg-2"/>
+                   <div className="col-md-4 col-lg-4"> <a href="#" className="btn btn-primary" role="button">Like !</a></div> 
+                  <div className="col-md-4 col-lg-4"> <a href="#" className="btn btn-success" role="button">Go !</a></div>
+                </div>
+              </div>
+            </div>
+        );
+      })}
+        </div>
+    </div> 
     );
   }
 }
